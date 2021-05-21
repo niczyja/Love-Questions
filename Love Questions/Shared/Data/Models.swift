26186fd154eaf98352738696c40d3cf696fc59fa
Maciej Sienkiewicz <maciej@niczyja.pl>
2021-05-21 22:59:45 +0200

    Initial project structure

---
+++ Love Questions/Shared/Data/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Love Questions
+//
+//  Created by Maciej Sienkiewicz on 21/05/2021.
+//
+
+import Foundation
---
+++ Love Questions/Shared/Love_QuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_QuestionsApp.swift
+//  Shared
+//
+//  Created by Maciej Sienkiewicz on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Love_QuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
